c661722525bf4037d690e238fb0c21742cab6f01
max <mwellh84@gmail.com>
2019-07-12 23:17:44 -0400

    set up auth signup and log in, add auth forms, add timeout to build user data into home page when signed in

---
+++ quiz_page_actions.js
@@ -0,0 +1,19 @@
+console.log('hi from quiz page actions script file')
+const displayWelcome = () => {
+  let message = document.querySelector('.messages-container')
+  let loggedInMessage = document.querySelector('.logged-in-message')
+  console.log('we are checking for a signed in user')
+  if(!!auth.currentUser) {
+    message.style.display = "flex"
+    loggedInMessage.textContent = `Welcome ${auth.currentUser.email}!`
+  }else{
+    message.style.display = "none"
+  }
+}
+
+window.addEventListener('load', (e) => {
+  setTimeout( () => {
+    console.log('we are running the timer')
+    displayWelcome()
+  }, 1000)
+})
